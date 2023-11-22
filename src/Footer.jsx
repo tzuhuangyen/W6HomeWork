0f4670fe76d5ce2e3c5f8dcb69ec33496660b621
tzuhuangyen <912yan@gmail.com>
2023-11-22 23:50:22 +0100

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,42 @@
+//class component
+import { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <footer className="footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <h3>Contact Us</h3>
+                <p>Email: yen@example.com</p>
+                <p>Phone: +1 123-456-7890</p>
+              </div>
+              <div className="col-md-6">
+                <div className="social-icons">
+                  <a href="#" target="_blank"></a>
+                  {/* <a href="#" target="_blank">
+                    <FontAwesomeIcon icon={faLinkedin} />{" "}
+                  </a>
+                  <a href="#" target="_blank">
+                    <FontAwesomeIcon icon={faFacebook} />{" "}
+                  </a>
+                  <a href="#" target="_blank">
+                    <FontAwesomeIcon icon={faInstagram} />{" "}
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+
+        <div className="footer-text">
+          &copy; 2023 Yen Website. All rights reserved.
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+//functional component
+import React from "react";
+
+const Header = function (props) {
+  return <h1>this is header</h1>;
+};
+
+export default Header;
