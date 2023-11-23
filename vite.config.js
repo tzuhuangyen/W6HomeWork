2aed38d02814849205d1777929e486be8c70d427
tzuhuangyen <912yan@gmail.com>
2023-11-23 11:13:35 +0100

    page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./W6HomeWork",
+  base: "/W6HomeWork",
 
   plugins: [react()],
 });
